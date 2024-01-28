0e7ebcdeb050fbe56981484696a17f647aad945d
yahiro <yahiro07@users.noreply.github.com>
2024-01-28 21:56:59 +0900

    直接配線キーに対応

--- kermite_rp_unified/src/main.cpp
+++ kermite_rp_unified/src/main.cpp
@@ -4,6 +4,7 @@
 #include <kpm/BoardLED_Dummy.h>
 #include <kpm/BoardLED_NeoPixel.h>
 #include <kpm/KeyMatrix.h>
+#include <kpm/KeyScanner_DirectWired.h>
 #include <kpm/KeyScanner_Dummy.h>
 
 typedef struct {
@@ -12,6 +13,7 @@ typedef struct {
   uint8_t boardLedType;
   uint8_t vlPinColumns[17];
   uint8_t vlPinRows[17];
+  uint8_t vlPinsDirectWired[17];
 } FirmixParams;
 
 volatile static const FirmixParams firmixParams = {
@@ -20,23 +22,37 @@ volatile static const FirmixParams firmixParams = {
     .boardLedType = 0,
     .vlPinColumns = {0},
     .vlPinRows = {0},
+    .vlPinsDirectWired = {0},
 };
 
 static KermiteCore kermite;
 static IKeyScanner *keyMatrix;
+static IKeyScanner *keyScannerDw;
 static IBoardLED *boardLED;
 
 static void setupModules() {
   int boardLedType = firmixParams.boardLedType;
   int numColumns = firmixParams.vlPinColumns[0];
   int numRows = firmixParams.vlPinRows[0];
+  int numDirectWiredPins = firmixParams.vlPinsDirectWired[0];
 
+  int keyIndexBase = 0;
   if (numColumns > 0 && numRows > 0) {
     auto *pinColumns = (const uint8_t *)firmixParams.vlPinColumns + 1;
     auto *pinRows = (const uint8_t *)firmixParams.vlPinRows + 1;
     keyMatrix = new KeyMatrix(pinColumns, pinRows, numColumns, numRows);
+    keyMatrix->setKeyIndexBase(keyIndexBase);
+    keyIndexBase += numColumns * numRows;
   } else {
     keyMatrix = new KeyScanner_Dummy();
+  }
+  if (numDirectWiredPins) {
+    auto pins = (const uint8_t *)firmixParams.vlPinsDirectWired + 1;
+    keyScannerDw = new KeyScanner_DirectWired(numDirectWiredPins, pins);
+    keyScannerDw->setKeyIndexBase(keyIndexBase);
+    keyIndexBase += numDirectWiredPins;
+  } else {
+    keyScannerDw = new KeyScanner_Dummy();
   }
 
   if (boardLedType == 1) {
@@ -69,6 +85,8 @@ void setup() {
   boardLED->initialize();
   keyMatrix->setKeyStateListener(handleKeyStateChange);
   keyMatrix->initialize();
+  keyScannerDw->setKeyStateListener(handleKeyStateChange);
+  keyScannerDw->initialize();
 
   if (firmixParams.keyboardName[0] != '\0') {
     kermite.setKeyboardName((const char *)firmixParams.keyboardName);
@@ -85,6 +103,7 @@ void loop() {
   boardLED->write(0, count % 1000 == 0);
   if (count % 10 == 0) {
     keyMatrix->updateInput();
+    keyScannerDw->updateInput();
   }
   kermite.processUpdate();
   count++;
